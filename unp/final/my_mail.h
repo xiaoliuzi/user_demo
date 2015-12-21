209af794b643c0099a347fcb2c5319171f6b6ac8
xiaoliuzi <qingpengliu@qq.com>
2015-12-21 16:38:19 +0800

    Emma, 12345, mail Alex hello, Alex, 12345, getmail Alex

---
+++ unp/final/my_mail.h
@@ -0,0 +1,17 @@
+
+struct buf{
+	int op;
+	int len;
+	char data[0];
+};
+
+
+struct buf* init_buf(int op, int len){
+	struct buf* that = (struct buf*)malloc(sizeof(struct buf) + sizeof(char)*len);
+	that->op = op;
+	that->len = len;
+
+	return that;
+}
+
+#define MAX_MAIL_LEN 1000
