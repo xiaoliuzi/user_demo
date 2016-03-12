1152101824d69ef5763a8f5fa3cbbc3734428db9
xiaoliuzi <qingpengliu@qq.com>
2016-03-12 14:56:59 +0800

    userbuffer_test

--- src/user_buffer_unpack.c
+++ src/user_buffer_unpack.c
@@ -6,100 +6,52 @@ gcc -lmsgpackc -pthread -o user_buffer_unpack user_buffer_unpack.c /usr/lib/libm
 #include <stdio.h>
 #include <assert.h>
 
-void msgpack_pack_string(msgpcka_packer *pk, char *str)
+#if 0
+void msgpack_pack_string(msgpack_packer *pk, char *str)
 {
 
 	msgpack_pack_str(&pk, strlen(str));
 	msgpack_pack_str_body(&pk, str, strlen(str));
 
 }
-
+#endif
 void prepare(msgpack_sbuffer* sbuf){
-	
 	msgpack_packer pk;
-	
 	msgpack_packer_init(&pk, sbuf, msgpack_sbuffer_write);
 
+	msgpack_pack_int(&pk, 2);
 
-#if 0	
-	/* 1st object */
-	msgpack_pack_array(&pk, 3);
-	msgpack_pack_int(&pk, 1);
-	msgpack_pack_true(&pk);
-	msgpack_pack_str(&pk, 7);
-	msgpack_pack_str_body(&pk, "example", 7);
-#endif	
-	/* 2nd objcet */
-	msgpack_pack_str(&pk, 4);
-	msgpack_pack_str_body(&pk, "mail", 4);
-	
 	msgpack_pack_str(&pk, 5);
 	msgpack_pack_str_body(&pk, "Alice", 5);
-
 	msgpack_pack_str(&pk, 10);
 	msgpack_pack_str_body(&pk, "helloworld", 10);
-#if 0	
-	/* 3rd objcet */
-	msgpack_pack_array(&pk, 2);
-	msgpack_pack_int(&pk, 42);
-	msgpack_pack_false(&pk);
-
-#endif
 }
 
 void unpack(char const* buf, size_t len) {
 	/* buf is allocated by client */
-//#if 0
 	msgpack_unpack_return ret;
 	msgpack_unpacked result; 
 	int i = 0;
 	size_t off = 0;
-	char *mt_content;// = "ab";
 	msgpack_unpacked_init(&result);
-
-	printf("buf is %s\n", buf);
-//	ret = msgpack_unpack_next(&result, buf, len, &off);
 	ret = msgpack_unpack_next(&result, buf, len, &off);
-//#endif
-
-
-	msgpack_unpacked mail_type;// wroten by xiaoliuzi 
-	size_t off_mt = 0;
-	msgpack_unpacked_init(&mail_type);
-	msgpack_unpack_next(&mail_type, mt_content, strlen(mt_content), &off_mt); 
-
 	while(ret == MSGPACK_UNPACK_SUCCESS) {
+
 		msgpack_object obj = result.data;
-	
+		obj.type = MSGPACK_OBJECT_STR;	
+	//	obj.via.str.size = strlen(buf);
 		/* use obj */
 		printf("Object no %d:\n", ++i);
-//		msgpack_object_print(stdout, obj);
-		printf(" obj is : ");
 		msgpack_object_print(stdout, obj);
 		printf("\n");
-
-		
-//		printf(" mail_type is : ");
-//		msgpack_object_print(stdout, mail_type.data);
+		printf("obj.vir.str.ptr is : %s\n", obj.via.str.ptr);
 		printf("\n");
-		
-		if ( (msgpack_object_equal(obj, mail_type.data)) == true ) {
-			printf(" the mail type is mail\n");
-		} 
-		else {
-			printf("the mail type is not mail\n");
-		}
-		
 
-
-		printf("\n");
 		/* If you wnat to allocate something on the zone, you can use zone. */
-
 		/* msgpack_zone* zone = result.zone; */
 		/* The lifetime of the obj and the zone. */
 		ret = msgpack_unpack_next(&result, buf, len, &off);
-		
-	}
+	}	
 	msgpack_unpacked_destroy(&result);
 
 	if (ret == MSGPACK_UNPACK_CONTINUE) {
@@ -115,14 +67,18 @@ int main(void) {
 
 	msgpack_sbuffer sbuf;
 	msgpack_sbuffer_init(&sbuf);
-	char *cmdstr="mail";	
-	prepare(&sbuf);
-		
-	unpack(sbuf.data, sbuf.size);
-//	unpack(cmdstr, strlen("mail"));
+	char cmdstr[100]="a5abcde";	
+
+	size_t len = strlen(cmdstr);
+
+//	prepare(&sbuf);
+	printf("sbuf.data = %d\n", sbuf.data);	
+//	unpack(cmdstr, 5);
+	sbuf.data = "\0xa5abcde";
+	unpack(sbuf.data, 6);
+
 	printf("----------------------\n");
 
-	printf("sbuf.data is : %x\n", sbuf.data);
 	msgpack_sbuffer_destroy(&sbuf);
 	
 	return 0;
